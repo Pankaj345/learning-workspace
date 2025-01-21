e6f082700ca53b901aab1e5b716f266474666561
Pankaj345 <ps1259237@gmail.com>
2025-01-21 23:34:10 +0530

    some more code

--- JavaScript/BasicJS/basic.js
+++ JavaScript/BasicJS/basic.js
@@ -1,2 +1,17 @@
 let user = "Lucky";
 console.log("hello", user,"Welcome to the world of JavaScript");
+// Alwayes give usefull & meaningful name to the variable
+// Always use camelCase for naming the variable
+
+let a = 20;
+let b = 20;
+let c = a + b;
+let d = a - b;
+console.log("Sum of a and b is",c);
+console.log("Substraction of a and b is",d);
+
+//basic sum of two numbers but this is long way can be done in short way
+console.log("Sum of a and b is",a+b);
+console.log("Substraction of a and b is",a-b);
+
+// solve some more problems
