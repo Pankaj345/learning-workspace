dc07e6598a851faff7840aa348448e088414a035
Pankaj345 <ps1259237@gmail.com>
2025-01-22 12:37:00 +0530

    remove some file

---
+++ JavaScript/Operators/ComparisonOperators.js
@@ -0,0 +1,6 @@
+//relation operators for only numbers but comparision operators for all types..
+
+console.log("bablu" == "bablu"); //true
+console.log(2 == 2);//true
+console.log("2" == "2");//true
+console.log("pankaj"=="Pankaj");//false case sensitive
